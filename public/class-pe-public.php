e489af0e5c70cc2064e5e2fd1b0ca4b36919f2dd
UjjawalWisdmlabs <ujjawal.srivastava@wisdmlabs.com>
2025-11-24 19:04:07 +0530

    Add HelpScout Beacon integration to footer in PE_Public class

--- public/class-pe-public.php
+++ public/class-pe-public.php
@@ -50,6 +50,7 @@ class PE_Public {
 	 */
 	public function init() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'reg_scripts_style' ) );
+		add_action( 'wp_footer', array( $this, 'add_helpscout_beacon' ) );
 		PE_Public_Enquiry_Button::instance();
 	}
 	/**
@@ -59,5 +60,15 @@ class PE_Public {
 		wp_register_style( 'wdm-juery-css', WDM_PE_PLUGIN_URL . 'assets/public/css/wdm-jquery-ui.css', array(), PEFREE_VERSION );
 		wp_register_script( 'wdm-validate', WDM_PE_PLUGIN_URL . 'assets/common/js/wdm_jquery.validate.min.js', array(), PEFREE_VERSION, true );
 		wp_register_script( 'wdm-contact', WDM_PE_PLUGIN_URL . 'assets/public/js/enquiry_validate.js', array( 'jquery' ), PEFREE_VERSION, true );
+	}
+
+	/**
+	 * Add HelpScout Beacon code to footer.
+	 */
+	public function add_helpscout_beacon() {
+		?>
+		<script type="text/javascript">!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});</script>
+		<script type="text/javascript">window.Beacon('init', 'fea56c43-0d44-4a4e-9715-1b1f20d6dcdf')</script>
+		<?php
 	}
 }
